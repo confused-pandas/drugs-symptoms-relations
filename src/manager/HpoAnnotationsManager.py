2623b1736b16503d14ced43f02e0872cf174a434
Najib Aghenda <najib.aghenda@gmail.com>
2018-05-05 10:02:18 +0200

    Manager hpo_annotations fini

--- src/manager/HpoAnnotationsManager.py
+++ src/manager/HpoAnnotationsManager.py
@@ -3,13 +3,13 @@ from sqlite3 import Error
 
 
 try:
-    conn = sqlite3.connect("/home/najib/Downloads/chinook.db")
+    conn = sqlite3.connect("./res/database/hpo/hpo_annotations.sqlite")
 except Error as e:
     print(e)
 
  
 cur = conn.cursor()
-cur.execute('SELECT * FROM albums')
+cur.execute('SELECT disease_db, disease_id, sign_id FROM phenotype_annotation;')
 rows = cur.fetchall()
 
 for row in rows:
