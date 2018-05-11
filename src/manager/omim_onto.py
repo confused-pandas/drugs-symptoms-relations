4b43a823f57cb368978da4845cabb0123168dc0f
Najib Aghenda <najib.aghenda@gmail.com>
2018-05-11 18:55:29 +0200

    Ajout omim_onto manager

--- src/manager/omim_onto.py
+++ src/manager/omim_onto.py
@@ -1,24 +1,30 @@
 import csv
 
-class OmimCsvManager:
+class OmimOntoManager:
 
-    def __init__(self, cui):
-        self.cui = cui
-        self.path = "./res/database/omim/omim_onto.csv"
+    def __init__(self, omim):
+        self.omim = omim
+        self.path = './res/database/omim/omim_onto.csv'
 
     def extractData(self):
         data_omimc = {}
-        with open(self.path, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                if row[5] == self.cui:
-                    if row[0][42:].startswith("MTHU"):
-                        data_omimc[self.cui] = row[0][46:52]
-                    else:
-                        data_omimc[self.cui] = row[0][46:52]
-            print(data_omimc) 
+        with open('./res/database/omim/omim_onto.csv','r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            b = False
+            while b != True:
+                row = csv_reader.__next__()
+                a = row[0][42:]
+                if a.startswith("MTHU"):
+                    if row[0][46:52] == self.omim:
+                        data_omimc[self.omim] = row[5]
+                        b = True
+                else:
+                    if row[0][42:48] == self.omim:
+                        data_omimc[self.omim] = row[5]
+                        b = True
+        print(data_omimc)
 
-                    
-                
-manager = OmimCsvManager("C1412749")
+
+
+manager = OmimOntoManager("600374")
 manager.extractData()
