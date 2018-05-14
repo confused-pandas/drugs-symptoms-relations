8966fa59731b7a30c438f5a82f7d42a26a6cd61f
Najib Aghenda <najib.aghenda@gmail.com>
2018-05-14 16:42:01 +0200

    Ajout d'ajout

--- src/manager/drugbank.py
+++ src/manager/drugbank.py
@@ -1,26 +1,46 @@
-from lxml import etree
+"""import os, os.path
+from whoosh.index import *
+from whoosh.fields import *
+from whoosh.qparser import QueryParser
+import xml.etree.ElementTree as ET
+
+class DrugBankManager:
+
+    def __init__(self, item):
+        self.item = item
+        self.path = './res/database/drugbank/full_database.xml'
+        self.path_index = "./res/database/drugbank/index_drug"
+        self.schema = Schema(name=TEXT(stored=True), indication=TEXT(stored=True), toxicity=TEXT(stored=True))
+
+    # Create the index
+    def index_initialisation(self):
+        if not os.path.exists(self.path_index):
+            os.mkdir(self.path_index)
+        ix = create_in(self.path_index, self.schema)
+        ix = open_dir(self.path_index)
+        writer = ix.writer()
+        tree = ET.parse(self.path)
+        root = tree.getroot()
+        for child in root:
+            print(child)
+
+        writer.commit()
+        
+    # Extract data from Omim.txt and store it in a dictionnary
+    def extractNameFromToxicity(self):
+        data_drug = {}                   
+        r = parserQuery(self, self.item, "toxicity")
+        for elem in r:
+            data_drug[elem.get("name")] = elem.get("")[7:].lstrip()
+        return data_drug
 
 
-context = etree.iterparse('./res/database/drugbank/full_database.xml', events=('end',), tag='Title')
+
+def parserQuery(self, item, schema_item):
+    ix = open_dir(self.path_index)
+    searcher = ix.searcher()
+    query = QueryParser(schema_item, ix.schema).parse(item)
+    results = searcher.search(query)
+    return results
 
 
-def fast_iter(context, func):
-    for event, elem in context:
-        func(elem)
-        elem.clear()
-        while elem.getprevious() is not None:
-            del elem.getparent()[0]
-    del context
-
-def func():
-    for event, elem in context:
-        out.write('%s\n' % elem.text.encode('utf-8'))        
-    
-        # It's safe to call clear() here because no descendants will be accessed
-        elem.clear()
-    
-        # Also eliminate now-empty references from the root node to <Title> 
-        while elem.getprevious() is not None:
-            del elem.getparent()[0]
-
-fast_iter(context, func())
