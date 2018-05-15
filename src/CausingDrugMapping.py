e41ac8058ce1204b5d4b6114e26054ef6a74b314
Najib Aghenda <najib.aghenda@gmail.com>
2018-05-15 12:41:11 +0200

    push

--- src/CausingDrugMapping.py
+++ src/CausingDrugMapping.py
@@ -1,6 +1,7 @@
 from manager.sider_se import SiderSEManager
 from manager.stitch import StitchManager
 from manager.atc import AtcManager
+from pymysql import cursors
 import sys
 
 class CausingDrugMapping:
@@ -9,24 +10,21 @@ class CausingDrugMapping:
         self.side_effect = side_effect
         
     def mapping(self):
-        #on récupère le CID correspondant au Side effect
         data_sider_se=SiderSEManager(self.side_effect).extractData()
         tabCID=[]
         for i in range(0, len(data_sider_se[self.side_effect])):
             tabCID.append(data_sider_se[self.side_effect][i]["stitch_compound_id2"])
         
         tabCIDunique=[]
-        tabCIDunique=(list(set(tabCID))) #•on élimine les doublons
+        tabCIDunique=(list(set(tabCID))) 
         
         for i in range(0, len(tabCIDunique)):
-            tabCIDunique[i]=tabCIDunique[i][3:] #on enlève les lettres CID
+            tabCIDunique[i]=tabCIDunique[i][4:]
         
         data_stitch={}
         for i in range(0, len(tabCIDunique)):
-            data_stitch[tabCIDunique[i]]=StitchManager(tabCIDunique[i])
-        
+            data_stitch[str(tabCIDunique[i])]=StitchManager(str(tabCIDunique[i])).extractDataFromStitchId()
         print(data_stitch)
-        
         
 CausingDrugMapping("Acute abdomen").mapping()
             
